ba9dff96fe0a03890445a070ea6320c081f8237c
AnatoleBusiness <mart.anatole@gmail.com>
2019-06-04 14:10:33 +0800

    simple node test + scene 1 created

---
+++ Assets/Scripts/BasicNode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicNode : MonoBehaviour
+{
+    GameObject movingPart;
+    GameObject goal;
+    float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        goal = this.transform.GetChild(0).gameObject;
+        movingPart = this.transform.GetChild(1).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movingPart.transform.localScale += new Vector3(speed/100, speed/100, 0);
+    }
+}
