8fceb198ccc66887fc652ef9613aa65a9cf2fabd
AnatoleBusiness <mart.anatole@gmail.com>
2019-06-04 16:40:55 +0800

    added Hand

--- Assets/Scripts/BasicNode.cs
+++ Assets/Scripts/BasicNode.cs
@@ -6,17 +6,49 @@ public class BasicNode : MonoBehaviour
 {
     GameObject movingPart;
     GameObject goal;
-    float speed = 1f;
+    GameObject rightHand;
+    float speed = 10f;
+    float size;
+    public GameObject hand;
+    bool inCircle = false;
     // Start is called before the first frame update
     void Start()
     {
         goal = this.transform.GetChild(0).gameObject;
         movingPart = this.transform.GetChild(1).gameObject;
+        rightHand = GameObject.Find("RightHand");
+        size = goal.transform.lossyScale.x;
+        hand.GetComponent<Rigidbody2D>().position = goal.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         movingPart.transform.localScale += new Vector3(speed/100, speed/100, 0);
+        //Debug.Log(size);
+        if (movingPart.transform.localScale.x >= size){
+            if (inCircle)
+                {
+                    Debug.Log("PERFECT");
+                } else {
+                    Debug.Log("MISSED");
+                }
+            Destroy(gameObject);
+        } 
     }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject == hand){
+            inCircle = true;
+        } 
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject == hand){
+            inCircle = false;
+        } 
+    }
+
 }
