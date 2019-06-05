bebda90f0547faa989a1cbb53a066e3f47402c39
AnatoleBusiness <mart.anatole@gmail.com>
2019-06-05 14:16:02 +0900

    added collsion recognition with simple node

--- Assets/Scripts/BasicNode.cs
+++ Assets/Scripts/BasicNode.cs
@@ -6,7 +6,6 @@ public class BasicNode : MonoBehaviour
 {
     GameObject movingPart;
     GameObject goal;
-    GameObject rightHand;
     float speed = 1f;
     float size;
     public GameObject hand;
@@ -16,16 +15,13 @@ public class BasicNode : MonoBehaviour
     {
         goal = this.transform.GetChild(0).gameObject;
         movingPart = this.transform.GetChild(1).gameObject;
-        rightHand = GameObject.Find("RightHand");
         size = goal.transform.lossyScale.x;
-        hand.GetComponent<Rigidbody2D>().position = goal.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         movingPart.transform.localScale += new Vector3(speed/100, speed/100, 0);
-        //Debug.Log(size);
         if (movingPart.transform.localScale.x >= size){
             if (inCircle)
                 {
@@ -40,8 +36,9 @@ public class BasicNode : MonoBehaviour
     void OnTriggerEnter2D(Collider2D col)
     {
         Debug.Log("Bonjour");
-        Debug.Log(col.gameObject);
-        if (col.gameObject == hand){
+        Debug.Log(col.gameObject.name);
+        if (col.gameObject.name == "HandRight"){
+            Debug.Log("I'm in");
             inCircle = true;
         } 
     }
