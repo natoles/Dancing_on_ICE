5d4c64556e2c4a13d41e84b527d6e0b73cdf2aec
AnatoleBusiness <mart.anatole@gmail.com>
2019-06-06 09:38:39 +0900

    basic node modifications

--- Assets/Scripts/BasicNode.cs
+++ Assets/Scripts/BasicNode.cs
@@ -6,10 +6,12 @@ public class BasicNode : MonoBehaviour
 {
     GameObject movingPart;
     GameObject goal;
-    float speed = 2f;
+    float speed = 0.5f;
     float size;
     public GameObject hand;
     bool inCircle = false;
+    float timeIn = 0;
+    public string member = "";
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +23,9 @@ public class BasicNode : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        movingPart.transform.Rotate(0,0,0.2f);
         movingPart.transform.localScale += new Vector3(speed/100, speed/100, 0);
-        if (movingPart.transform.localScale.x >= size){
+        if (movingPart.transform.lossyScale.x >= size - size/12){
             if (inCircle)
                 {
                     Debug.Log("PERFECT");
@@ -35,14 +38,15 @@ public class BasicNode : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.tag == "Hand"){
+        if (col.gameObject.tag == member){
             inCircle = true;
+            timeIn = Time.time;
         } 
     }
 
     void OnTriggerExit2D(Collider2D col)
     {
-        if (col.gameObject.tag == "Hand"){
+        if (col.gameObject.tag == member){
             inCircle = false;
         } 
     }
