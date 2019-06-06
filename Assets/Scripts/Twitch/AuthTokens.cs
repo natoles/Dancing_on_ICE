7f5e8f0ad2ce1e650a7985499fa25302d504c0ba
Twifus <43961858+Twifus@users.noreply.github.com>
2019-06-06 17:09:37 +0900

    Removed useless usings in AuthTokens

--- Assets/Scripts/Twitch/AuthTokens.cs
+++ Assets/Scripts/Twitch/AuthTokens.cs
@@ -1,12 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class AuthTokens
+﻿public static class AuthTokens
 {
+    public static string BOT_NAME           { get; } = "dancing_on_ice";
     public static string CLIENT_ID          { get; } = "";
     public static string CLIENT_SECRET      { get; } = "";
     public static string BOT_ACCESS_TOKEN   { get; } = "";
     public static string BOT_REFRESH_TOKEN  { get; } = "";
-    public static string BOT_NAME           { get; } = "dancing_on_ice";
 }
