30b2e3ab1819d4dece15aacd112760b082c65816
AnatoleBusiness <mart.anatole@gmail.com>
2019-06-06 10:56:50 +0900

    Added time-based movement for the basicNode

--- Assets/Scripts/BasicNode.cs
+++ Assets/Scripts/BasicNode.cs
@@ -4,27 +4,37 @@ using UnityEngine;
 
 public class BasicNode : MonoBehaviour
 {
-    GameObject movingPart;
-    GameObject goal;
-    float speed = 1f;
-    float size;
-    bool inCircle = false;
-    float timeIn = 0;
-    float timeFrame = 1f; //Time to make a PERFECT
-    string member = "Hand";
+    GameObject movingPart; //Inner circle
+    GameObject goal; //Outer circle
+    Vector3 size; //Size of the outer circle 
+    bool inCircle = false; //True if the correct joint is in the node
+    float timeIn = 0; //Time since the joint has entered the node
+    float timeFrame; //Time frame to make a PERFECT
+    float timeToFinish = 3f; ///Time the node will take to destroy itself
+    string member = "Hand"; //Choice of the joint who will activate the node (Hand or Foot)
+    private IEnumerator _growth; 
+    bool finished = false; //True if the node is finished
     
     void Start()
     {
         goal = this.transform.GetChild(0).gameObject;
         movingPart = this.transform.GetChild(1).gameObject;
-        size = goal.transform.lossyScale.x;
+        size = goal.transform.localScale;
+        timeFrame = timeToFinish/10;
+
+        //Graphical adjustment
+        size[0] -= size[0]/12;
+        size[1] -= size[1]/12;
+
+        _growth = Growth(3f);
+        StartCoroutine(_growth);
     }
 
     
     void Update()
     {
-        movingPart.transform.localScale += new Vector3(speed/100, speed/100, 0);
-        if (movingPart.transform.lossyScale.x >= size - size/12){
+        //Scroe according to timing
+        if (finished){
             if (inCircle)
                 {
                     if (Time.time - timeIn <= timeFrame)
@@ -40,10 +50,8 @@ public class BasicNode : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        //Debug.Log("Hey");
         if (col.gameObject.tag == member){
             inCircle = true;
-            //Debug.Log("I'm in");
             timeIn = Time.time;
         } 
     }
@@ -53,6 +61,20 @@ public class BasicNode : MonoBehaviour
         if (col.gameObject.tag == member){
             inCircle = false;
         } 
+    }
+
+    //Interpolate the inner circle
+    private IEnumerator Growth(float timeGrowth){
+        float progress = 0;
+        Vector3 initialScale = movingPart.transform.localScale;
+        Vector3 finalScale = size;
+        while(progress <= timeGrowth){
+            movingPart.transform.localScale = Vector3.Lerp(initialScale, finalScale, progress/timeGrowth);
+            progress += Time.deltaTime;
+            yield return null;
+        }
+        movingPart.transform.localScale = finalScale;
+        finished = true;
     }
 
 }
