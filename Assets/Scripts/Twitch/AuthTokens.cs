bec8e0fe71d135d9dc6ae0b11be78a7e53019011
Twifus <43961858+Twifus@users.noreply.github.com>
2019-06-06 09:52:41 +0900

    Ajout botname dans authokens

--- Assets/Scripts/Twitch/AuthTokens.cs
+++ Assets/Scripts/Twitch/AuthTokens.cs
@@ -8,4 +8,5 @@ public static class AuthTokens
     public static string CLIENT_SECRET      { get; } = "";
     public static string BOT_ACCESS_TOKEN   { get; } = "";
     public static string BOT_REFRESH_TOKEN  { get; } = "";
+    public static string BOT_NAME           { get; } = "dancing_on_ice";
 }
