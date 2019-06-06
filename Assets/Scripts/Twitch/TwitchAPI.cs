85d725681bb6466f2b86fb8b7369247c6163d0a3
Twifus <43961858+Twifus@users.noreply.github.com>
2019-06-06 17:10:50 +0900

    First Test TwitchApi for viewers count

---
+++ Assets/Scripts/Twitch/TwitchAPI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TwitchLib.Api.Models.Helix.Streams.GetStreams;
+using UnityEngine;
+using Twitch = TwitchLib.Unity;
+
+public class TwitchAPI : MonoBehaviour
+{
+    private Twitch.Api api;
+
+    private void Start()
+    {
+        api = new Twitch.Api();
+        api.Settings.AccessToken = AuthTokens.BOT_ACCESS_TOKEN;
+        api.Settings.ClientId = AuthTokens.CLIENT_ID;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartCoroutine(GetViewersCount());
+        }
+    }
+
+    private IEnumerator GetViewersCount()
+    {
+        GetStreamsResponse obj = null;
+        yield return
+            api.InvokeAsync(
+                api.Streams.helix.GetStreamsAsync(userLogins: new List<string> { TwitchClient.Instance().JoinedChannels[0].Channel }),
+                (response) => obj = response
+                );
+
+        if (obj.Streams.Length == 0)
+            Debug.Log("User not live");
+        else
+            Debug.Log($"{obj.Streams[0].ViewerCount} viewers on stream");
+    }
+}
