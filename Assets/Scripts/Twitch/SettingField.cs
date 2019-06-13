19fb1cc01832012a442132327320d58d6e6ad445
Twifus <43961858+Twifus@users.noreply.github.com>
2019-06-13 16:36:25 +0900

    Changed CommandSelector to SettingField

---
+++ Assets/Scripts/Twitch/SettingField.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+[CustomEditor(typeof(SettingField))]
+public class SettingFieldEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+}
+
+
+[Serializable]
+public class SettingField : InputField
+{
+    [SerializeField]
+    protected SettingTyp type = SettingTyp.AudTimCmd;
+
+    protected override void Start()
+    {
+        base.Start();
+        if (Application.isPlaying) // prevent this part from being executed in EditMode (because TwitchClient is not instancied at this time)
+        {
+            text = SettingsManager.Instance.twitch[type].value;
+            onValueChanged.AddListener(UpdateCommand);
+        }
+    }
+
+    private void UpdateCommand(string arg0)
+    {
+        SettingsManager.Instance.twitch[type].value = arg0;
+    }
+}
