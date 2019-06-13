e2361916d2fa5b2d2f34fa0029a839780fd16a59
Twifus <43961858+Twifus@users.noreply.github.com>
2019-06-13 14:32:39 +0900

    Improved SettingsManager for code clarity + added TwitchUsername

--- Assets/Scripts/Twitch/SettingsManager.cs
+++ Assets/Scripts/Twitch/SettingsManager.cs
@@ -1,44 +1,71 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using SharpConfig;
 
+public enum SettingTyp
+{
+    TwitchUsr,  // Twitch Username
+    AudTimCmd,  // Audience Time
+    IncDiffCmd, // Increase difficulty
+    DecDiffCmd, // Decrease difficulty
+    CongratCmd  // Congratulate
+}
+
 public class SettingsManager : Singleton<SettingsManager>
 {
-    public enum CommandType
+    // Twitch-related settings
+    private readonly string twitchSection = "Twitch";
+    public readonly Dictionary<SettingTyp, StringSetting> twitch = new Dictionary<SettingTyp, StringSetting>
     {
-        AudienceTime,
-        IncreaseDifficulty,
-        DecreaseDifficulty,
-        Congratulate
-    }
+        //  Command Type                                Config file field               Default value
+        {   SettingTyp.TwitchUsr,   new StringSetting(  "TwitchUsername"            ,   ""            ) },
+        {   SettingTyp.AudTimCmd,   new StringSetting(  "AudienceTimeCommand"       ,   "gambatte"    ) },
+        {   SettingTyp.IncDiffCmd,  new StringSetting(  "IncreaseDifficultyCommand" ,   "increase"    ) },
+        {   SettingTyp.DecDiffCmd,  new StringSetting(  "DecreaseDifficultyCommand" ,   "decrease"    ) },
+        {   SettingTyp.CongratCmd,  new StringSetting(  "CongratulationCommand"     ,   "gg"          ) },
+    };
 
-    private string path;
-    public Configuration config = null;
+    #region Implementation
+    private string path; // setting file path
+
+    public class StringSetting
+    {
+        internal string setting;
+        public string value;
+
+        public StringSetting(string s, string v)
+        {
+            setting = s;
+            value = v;
+        }
+    }
 
     private void Awake()
     {
         path = $"{Application.persistentDataPath}/config.cfg";
-        Debug.Log(path);
         if (System.IO.File.Exists(path))
         {
-            config = Configuration.LoadFromFile(path);
-        }
-        else
-        {
-            config = new Configuration();
-            config["ChatCommands"]["AudienceTimeCommand"]       .StringValue = "gambatte";
-            config["ChatCommands"]["IncreaseDifficultyCommand"] .StringValue = "increase";
-            config["ChatCommands"]["DecreaseDifficultyCommand"] .StringValue = "decrease";
-            config["ChatCommands"]["CongratulationCommand"]     .StringValue = "gg";
-            SaveConfig();
+            Configuration config = Configuration.LoadFromFile(path);
+            
+            foreach (KeyValuePair<SettingTyp, StringSetting> c in twitch)
+            {
+                SettingTyp typ = c.Key;
+                c.Value.value = config[twitchSection][c.Value.setting].StringValue;
+            }
         }
     }
 
     private void SaveConfig()
     {
+        Configuration config = new Configuration();
+        foreach (KeyValuePair<SettingTyp, StringSetting> c in twitch)
+        {
+            SettingTyp typ = c.Key;
+            config[twitchSection][c.Value.setting].StringValue = c.Value.value;
+        }
         config.SaveToFile(path);
-        Debug.Log(config);
     }
 
     private void OnApplicationQuit()
@@ -50,4 +77,6 @@ public class SettingsManager : Singleton<SettingsManager>
     {
         SaveConfig();
     }
+
+    #endregion
 }
