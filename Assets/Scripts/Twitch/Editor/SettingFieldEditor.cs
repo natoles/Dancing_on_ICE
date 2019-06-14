cdb9d94d4847d765d313c0da39321a58beabed95
Twifus <43961858+Twifus@users.noreply.github.com>
2019-06-14 12:26:34 +0900

    Updated CustomEditor for SettingField

---
+++ Assets/Scripts/Twitch/Editor/SettingFieldEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(SettingField), true)]  
+public class SettingFieldEditor : InputFieldEditor
+{
+    SerializedProperty m_SettingType;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        m_SettingType = serializedObject.FindProperty("m_SettingType");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.PropertyField(m_SettingType);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
--- Assets/Scripts/Twitch/SettingField.cs
+++ Assets/Scripts/Twitch/SettingField.cs
@@ -5,34 +5,24 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEditor;
 
-[CustomEditor(typeof(SettingField))]
-public class SettingFieldEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
-    }
-}
-
-
 [Serializable]
 public class SettingField : InputField
 {
     [SerializeField]
-    protected SettingTyp type = SettingTyp.AudTimCmd;
+    protected SettingTyp m_SettingType = SettingTyp.AudTimCmd;
 
     protected override void Start()
     {
         base.Start();
         if (Application.isPlaying) // prevent this part from being executed in EditMode (because TwitchClient is not instancied at this time)
         {
-            text = SettingsManager.Instance.twitch[type].value;
+            text = SettingsManager.Instance.twitch[m_SettingType].value;
             onValueChanged.AddListener(UpdateCommand);
         }
     }
 
     private void UpdateCommand(string arg0)
     {
-        SettingsManager.Instance.twitch[type].value = arg0;
+        SettingsManager.Instance.twitch[m_SettingType].value = arg0;
     }
 }
