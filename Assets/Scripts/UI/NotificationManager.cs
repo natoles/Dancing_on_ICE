1eb0078d1a20f93cc987636c5e4970f4d40c9fa1
Twifus <43961858+Twifus@users.noreply.github.com>
2019-06-17 15:50:06 +0900

    Added generic notification system

---
+++ Assets/Scripts/UI/NotificationManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using TwitchLib.Client.Events;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotificationManager : Singleton<NotificationManager>
+{
+    public NotificationPanel notificationPanelPrefab;
+
+    private string channel = null;
+
+    bool quitting = false;
+
+    protected void Start()
+    {
+        TwitchClient.Instance.OnJoinedChannel += Notifier_OnJoinedChannel;
+        TwitchClient.Instance.OnLeftChannel += Notifier_OnLeftChannel;
+        TwitchClient.Instance.OnDisconnected += Notifier_OnDisconnected;
+    }
+
+    public void PushNotification(string text, Color textColor, Color bgColor)
+    {
+        NotificationPanel notif = Instantiate<NotificationPanel>(notificationPanelPrefab);
+        notif.name = "NotificationPanel";
+        notif.rectTransform.SetParent(transform);
+        notif.textComponent.text = text;
+        notif.textComponent.color = textColor;
+        notif.color = bgColor;
+        notif.Trigger();
+    }
+
+    private void Notifier_OnJoinedChannel(object sender, TwitchLib.Client.Events.OnJoinedChannelArgs e)
+    {
+        PushNotification("Connected to channel " + e.Channel, Color.white, Color.green);
+        channel = e.Channel;
+    }
+
+    private void Notifier_OnLeftChannel(object sender, TwitchLib.Client.Events.OnLeftChannelArgs e)
+    {
+        if (channel != null)
+        {
+            PushNotification("Disconnected from channel " + channel, Color.white, Color.red);
+            channel = null;
+        }
+    }
+
+    private void Notifier_OnDisconnected(object sender, OnDisconnectedArgs e)
+    {
+        if (channel != null)
+        {
+            PushNotification("Disconnected from channel " + channel, Color.white, Color.red);
+            channel = null;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        quitting = true;
+    }
+
+    protected void OnDestroy()
+    {
+        if (!quitting) // prevent this part from being executed in EditMode or while exiting (because TwitchClient may be not instancied at this time)
+        {
+            TwitchClient.Instance.OnJoinedChannel -= Notifier_OnJoinedChannel;
+            TwitchClient.Instance.OnLeftChannel -= Notifier_OnLeftChannel;
+            TwitchClient.Instance.OnDisconnected -= Notifier_OnDisconnected;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/NotificationPanel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using TwitchLib.Client.Events;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotificationPanel : Image
+{
+    public Text textComponent { get; private set; } = null;
+
+    private float startTime = float.MaxValue - 1;
+    private float stopTime = float.MaxValue;
+
+    [SerializeField]
+    private float transitionTime = 0.3f;
+    
+    [SerializeField]
+    private float displayTime = 2.5f;
+    
+    [SerializeField]
+    private Vector3 initialPosition = new Vector3(0, 0, 0);
+
+    [SerializeField]
+    private Vector3 finalPosition = new Vector3(0, 40, 0);
+
+    protected override void Awake()
+    {
+        textComponent = GetComponentInChildren<Text>();
+    }
+    
+    public void Trigger()
+    {
+        startTime = Time.time;
+        stopTime = startTime + 2 * transitionTime + displayTime;
+    }
+
+    private void Update()
+    {
+        float coeff = 0;
+        if (Time.time < startTime)
+        {
+            return;
+        }
+        else
+        {
+            if (Time.time <= startTime + transitionTime + displayTime / 2)
+            {
+                coeff = (Time.time - startTime) / transitionTime;
+            }
+            else if (Time.time <= stopTime)
+            {
+                coeff = (stopTime - Time.time) / transitionTime;
+            }
+            else
+            {
+                Destroy(gameObject);
+                return;
+            }
+        }
+        rectTransform.anchoredPosition = Vector3.Lerp(initialPosition, finalPosition, coeff);
+    }
+}
