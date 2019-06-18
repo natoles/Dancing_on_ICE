bada449e589e9525eeba56020516127d81e487fd
Twifus <43961858+Twifus@users.noreply.github.com>
2019-06-18 17:10:48 +0900

    Clean ChatDisplay script

--- Assets/Scripts/Twitch/ChatDisplay.cs
+++ Assets/Scripts/Twitch/ChatDisplay.cs
@@ -10,7 +10,7 @@ public class ChatDisplay : MonoBehaviour
 {
     private Text chat = null;
     public InputField twitchChannelInputField = null;
-    public Button connectButton = null;
+    public ConnectButton connectButton = null;
 
 
     private void Start()
@@ -18,54 +18,7 @@ public class ChatDisplay : MonoBehaviour
         chat = GetComponent<Text>();
         TwitchClient.Instance.OnMessageReceived += Chat_OnMessageReceived;
         twitchChannelInputField.text = SettingsManager.Instance.twitch[SettingTyp.TwitchUsr].value;
-        twitchChannelInputField.onValueChanged.AddListener(delegate { ConnectLayout(); });
-        //connectButton.onClick.AddListener(ConnectToChannelWrapper);
-    }
-
-    private void ConnectToChannelWrapper()
-    {
-        StartCoroutine(ConnectToChannel());
-    }
-
-    private IEnumerator ConnectToChannel()
-    {
-        GetStreamsResponse obj = null;
-        yield return
-            TwitchAPI.Instance.InvokeAsync(
-                TwitchAPI.Instance.Streams.helix.GetStreamsAsync(userLogins: new List<string> { twitchChannelInputField.text }),
-                (response) => obj = response
-                );
-        
-        if (obj.Streams.Length > 0)
-        {
-            Debug.Log($"{obj.Streams[0].ViewerCount} viewers on stream");
-            TwitchClient.Instance.ConnectTo(twitchChannelInputField.text);
-            TwitchClient.Instance.OnJoinedChannel += (sender, e) => { ConnectedLayout(); } ;
-        }
-        else
-        {
-            Debug.Log("User not live");
-            twitchChannelInputField.textComponent.color = Color.red;
-        }
-    }
-
-    private void ConnectLayout()
-    {
-        twitchChannelInputField.textComponent.color = Color.black;
-        Text buttonText = connectButton.GetComponentInChildren<Text>();
-        buttonText.text = "Connect";
-        buttonText.color = Color.black;
-        connectButton.interactable = true;
-        SettingsManager.Instance.twitch[SettingTyp.TwitchUsr].value = twitchChannelInputField.text;
-    }
-
-    private void ConnectedLayout()
-    {
-        twitchChannelInputField.textComponent.color = Color.black;
-        Text buttonText = connectButton.GetComponentInChildren<Text>();
-        buttonText.text = "Connected";
-        buttonText.color = Color.green;
-        connectButton.interactable = false;
+        twitchChannelInputField.onValueChanged.AddListener(delegate { connectButton.ChangeButtonLayout("Connect", Color.green, true); });
     }
 
     private void Chat_OnMessageReceived(object sender, OnMessageReceivedArgs e)
