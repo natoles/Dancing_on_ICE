4cbc505473827874c4046f921dadf14d14e2fb52
Twifus <43961858+Twifus@users.noreply.github.com>
2019-06-18 17:10:38 +0900

    ConnectButton no longer check if channel is live

--- Assets/Scripts/Twitch/ConnectButton.cs
+++ Assets/Scripts/Twitch/ConnectButton.cs
@@ -13,9 +13,11 @@ public class ConnectButton : Button
 
     protected override void Awake()
     {
+        interactable = false;
         base.Awake();
+        
         text = GetComponentInChildren<Text>();
-        ChangeButtonLayout("Connect", Color.green);
+        ChangeButtonLayout("Connect", Color.green, true);
 
         if (Application.isPlaying) // prevent this part from being executed in EditMode (because TwitchClient is not instancied at this time)
         {
@@ -23,92 +25,79 @@ public class ConnectButton : Button
             TwitchClient.Instance.OnJoinedChannel += ConnectButton_OnJoinedChannel;
             TwitchClient.Instance.OnLeftChannel += ConnectButton_OnLeftChannel;
             TwitchClient.Instance.OnDisconnected += ConnectButton_OnDisconnected;
+            TwitchClient.Instance.OnFailureToReceiveJoinConfirmation += ConnectButton_OnJoinFailed;
             if (TwitchClient.Instance.IsConnected)
-                ChangeButtonLayout("Disconnect", Color.red);
+                ChangeButtonLayout("Disconnect", Color.red, true);
         }
+
+        interactable = true;
     }
 
     private void ConnectButton_OnClickHandler()
     {
-        string user = SettingsManager.Instance.twitch[SettingTyp.TwitchUsr].value;
-        if (user == null || user == string.Empty)
-        {
-            return;
-        }
+        string channel = SettingsManager.Instance.twitch[SettingTyp.TwitchUsr].value;
 
-        interactable = false;
         if (!TwitchClient.Instance.IsConnected)
         {
-            ChangeButtonLayout("Connecting", Color.grey);
-            StartCoroutine(ConnectToStream(user));
+            if (TwitchClient.Instance.JoinChannel(channel))
+            {
+                ChangeButtonLayout("Connecting", Color.grey, false);
+            }
         }
         else
         {
-            ChangeButtonLayout("Disconnecting", Color.grey);
-            TwitchClient.Instance.Disconnect();
+            if (TwitchClient.Instance.LeaveChannel())
+                ChangeButtonLayout("Disconnecting", Color.grey, false);
         }
     }
 
-    private IEnumerator ConnectToStream(string stream)
-    {
-        GetStreamsResponse obj = null;
-        yield return
-            TwitchAPI.Instance.InvokeAsync(
-                TwitchAPI.Instance.Streams.helix.GetStreamsAsync(userLogins: new List<string> { stream }),
-                (response) => obj = response
-                );
+    //private IEnumerator ConnectToStream(string stream)
+    //{
+    //    GetStreamsResponse obj = null;
+    //    yield return
+    //        TwitchAPI.Instance.InvokeAsync(
+    //            TwitchAPI.Instance.Streams.helix.GetStreamsAsync(userLogins: new List<string> { stream }),
+    //            (response) => obj = response
+    //            );
 
-        if (obj.Streams.Length > 0)
-        {
-            Debug.Log($"{obj.Streams[0].ViewerCount} viewers on stream");
-            TwitchClient.Instance.ConnectTo(stream);
-        }
-        else
-        {
-            Debug.Log("User not live");
-            ChangeButtonLayout("Connect", Color.green);
-            interactable = true;
-        }
-    }
+    //    if (obj.Streams.Length > 0)
+    //    {
+    //        Debug.Log($"{obj.Streams[0].ViewerCount} viewers on stream");
+    //        TwitchClient.Instance.JoinChannel(stream);
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("User not live");
+    //        NotificationManager.Instance.PushNotification("User not live", Color.white, Color.blue);
+    //        ChangeButtonLayout("Connect", Color.green, true);
+    //    }
+    //}
 
     private void ConnectButton_OnJoinedChannel(object sender, OnJoinedChannelArgs e)
     {
-        ChangeButtonLayout("Disconnect", Color.red);
-        interactable = true;
+        ChangeButtonLayout("Disconnect", Color.red, true);
     }
 
     private void ConnectButton_OnLeftChannel(object sender, OnLeftChannelArgs e)
     {
-        ChangeButtonLayout("Connect", Color.green);
-        interactable = true;
+        ChangeButtonLayout("Connect", Color.green, true);
     }
 
     private void ConnectButton_OnDisconnected(object sender, OnDisconnectedArgs e)
     {
-        ChangeButtonLayout("Connect", Color.green);
-        interactable = true;
+        ChangeButtonLayout("Connect", Color.green, true);
     }
 
-    private void ChangeButtonLayout(string text, Color color)
+    private void ConnectButton_OnJoinFailed(object sender, OnFailureToReceiveJoinConfirmationArgs e)
+    {
+        ChangeButtonLayout("Connect", Color.green, true);
+    }
+
+    public void ChangeButtonLayout(string text, Color color, bool interactable)
     {
         this.text.text = text;
         this.text.color = color;
-    }
-
-    private void Update()
-    {
-        if (Application.isPlaying)
-        {
-            string user = SettingsManager.Instance.twitch[SettingTyp.TwitchUsr].value;
-            if (user == null || user == string.Empty)
-            {
-                interactable = false;
-            }
-            else
-            {
-                interactable = true;
-            }
-        }
+        this.interactable = interactable;
     }
 
     private void OnApplicationQuit()
