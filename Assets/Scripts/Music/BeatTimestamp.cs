b3a4def1c054ddefad62cce6b4810407f472c745
Twifus <43961858+Twifus@users.noreply.github.com>
2019-06-21 20:53:12 +0900

    Beatmap structures

---
+++ Assets/Scripts/Music/BeatTimestamp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+public enum BeatType
+{
+    Simple,
+    Slider
+}
+
+[Serializable]
+public class BeatTimestamp
+{
+    public BeatType type = BeatType.Simple;
+    public float time = -1;
+    public float duration = 0;
+}
+
+[Serializable]
+public class BeatMap
+{
+    public List<BeatTimestamp> Pool1 = new List<BeatTimestamp>();
+    public List<BeatTimestamp> Pool2 = new List<BeatTimestamp>();
+}
