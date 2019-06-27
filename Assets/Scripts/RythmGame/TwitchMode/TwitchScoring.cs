3299622fad0b978b057e9df463290be082804f14
Twifus <43961858+Twifus@users.noreply.github.com>
2019-06-27 16:42:16 +0900

    Twitch scoring using chat messages (untested)

---
+++ Assets/Scripts/RythmGame/TwitchMode/TwitchScoring.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TwitchLib.Client.Events;
+using UnityEngine;
+
+public class TwitchScoring : Scoring
+{
+
+    private void Start()
+    {
+        TwitchClient.Instance.OnMessageReceived += Increase_Score;
+    }
+
+    private void Increase_Score(object sender, OnMessageReceivedArgs e)
+    {
+        if (e.ChatMessage.Message == SettingsManager.Instance.twitch[SettingTyp.AudTimCmd].value)
+        {
+            multiplier += 0.1f;
+        }
+    }
+}
