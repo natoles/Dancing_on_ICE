a8cf57709959f45c932498c7bb3b21ed3a21d310
Twifus <43961858+Twifus@users.noreply.github.com>
2019-06-27 14:27:19 +0900

    Better score update

--- Assets/Scripts/RythmGame/Scoring.cs
+++ Assets/Scripts/RythmGame/Scoring.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class Scoring : MonoBehaviour
 {
-    public int score = 0; //Actual score
-    public int tmpScore = 0; //Displayed score
+    public int scoreUpdateTickValue = 127; //tmpScore is incresed by this value each tick
+    public float scoreUpdateMaxDelay = 1f; //updating tmpScore cannot take longer than this delay
+
+    public float score = 0; //Actual score
+    public float tmpScore = 0; //Displayed score
+    public float multiplier = 1f; //Score multiplier
+    
+    private float targetTimeUpdated = 0f; //time when tmpScore must have been updated
+    private bool updated = true; //is tmpScore updated? (ie. score == tmpScore)
 
     // If displayed score is less than real score, increment it
     void Update()
     {
-        if(tmpScore < score - 151){
-            tmpScore += 151;
-        } else {
-            if ((tmpScore < score))
-            tmpScore += score-tmpScore;
+        if (!updated)
+        {
+            tmpScore = Mathf.Min(score, tmpScore + Mathf.Max(scoreUpdateTickValue, (score - tmpScore) * Time.deltaTime / (targetTimeUpdated - Time.time)));
+
+            if (Time.time >= targetTimeUpdated)
+            {
+                tmpScore = score;
+                updated = true;
+            }
         }
     }
 
     //in case of point multiplicator add it here
-    public void AddScore(int scoreAdded){
-        score += scoreAdded;
+    public void AddScore(float scoreAdded){
+        score += scoreAdded * multiplier;
+        targetTimeUpdated = Time.time + scoreUpdateMaxDelay;
+        updated = false;
     }
 }
