f3f3813659766f35f1a30a92521986d2166d861a
Twifus <43961858+Twifus@users.noreply.github.com>
2019-06-28 14:44:32 +0900

    Added song folder scan to music selection screen

--- Assets/Scripts/SongSelection/SongScrollView.cs
+++ Assets/Scripts/SongSelection/SongScrollView.cs
@@ -1,10 +1,27 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class SongScrollView : ScrollRect
 {
+    [SerializeField]
+    protected SongEntry m_SongEntry = null;
 
     protected override void Start()
     {
+        if (Application.isPlaying)
+        {
+            string[] songFolders = Directory.GetDirectories(Application.streamingAssetsPath + "/Songs");
+            foreach (string songFolder in songFolders)
+            {
+                string[] bmFiles = Directory.GetFiles(songFolder, "*.icebm");
+                foreach (string bmFile in bmFiles)
+                {
+                    SongEntry entry = Instantiate(m_SongEntry, content);
+                    entry.gameObject.name = Path.GetFileNameWithoutExtension(bmFile);
+                    entry.SetSong(bmFile);
+                }
+            }
+        }
     }
 }
