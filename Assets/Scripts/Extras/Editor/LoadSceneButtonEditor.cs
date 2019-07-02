675dc01e7506d14d03de568ea4eaacfb30d5cfef
Twifus <43961858+Twifus@users.noreply.github.com>
2019-07-02 13:12:16 +0900

    Simple Title screen to access Yoga and Twitch modes

---
+++ Assets/Scripts/Extras/Editor/LoadSceneButtonEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.UI;
+using UnityEngine.SceneManagement;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(LoadSceneButton), true)]
+public class LoadSceneButtonEditor : ButtonEditor
+{
+    SerializedProperty ScenePath;
+    int selected = 0;
+    List<string> options = new List<string>();
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        ScenePath = serializedObject.FindProperty("ScenePath");        
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            options.Add(scene.path);
+        }
+        selected = options.FindIndex((string str) => str == ScenePath.stringValue);
+        selected = selected >= 0 ? selected : 0;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        selected = EditorGUILayout.Popup("Scene To Load", selected, options.ToArray());
+        ScenePath.stringValue = options[selected];
+
+        serializedObject.ApplyModifiedProperties();
+
+        EditorGUILayout.Space();
+
+        base.OnInspectorGUI();
+    }
+}
