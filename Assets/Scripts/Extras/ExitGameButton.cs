327f76b0706aca043457bd61f7bbcb402364866c
Twifus <43961858+Twifus@users.noreply.github.com>
2019-07-02 13:26:59 +0900

    Added exit button to title screen

---
+++ Assets/Scripts/Extras/ExitGameButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ExitGameButton : Button, IPointerClickHandler
+{
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        base.OnPointerClick(eventData);
+        Application.Quit();
+    }
+}
