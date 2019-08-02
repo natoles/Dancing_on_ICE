c180c963667450650d4aefa4ff47b5e169d57822
Twifus <43961858+Twifus@users.noreply.github.com>
2019-08-02 16:42:06 +0900

    Improved mode handling

---
+++ Assets/Scripts/RythmGame/Modes/Editor/ModesManagerEditor.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+namespace DancingICE.Modes
+{
+    [CustomEditor(typeof(ModeManager), true)]
+    public class ModeManagerEditor : Editor
+    {
+        SerializedProperty Modes;
+        SerializedProperty Current;
+
+        ReorderableList ModesList;
+
+        protected void OnEnable()
+        {
+            Modes = serializedObject.FindProperty("modes");
+            Current = serializedObject.FindProperty("current");
+            ModesList = new ReorderableList(serializedObject, Modes)
+            {
+                draggable = true,
+                displayAdd = true,
+                displayRemove = true,
+                drawHeaderCallback = (rect) => EditorGUI.LabelField(rect, "Game modes"),
+                drawElementCallback = (rect, index, active, focused) =>
+                {
+                    SerializedProperty element = Modes.GetArrayElementAtIndex(index);
+
+                    SerializedProperty Name = element.FindPropertyRelative("name");
+                    SerializedProperty Slider = element.FindPropertyRelative("showDifficultySlider");
+                    SerializedProperty Buttons = element.FindPropertyRelative("buttonsToShow");
+                    SerializedProperty SceneToLoad = element.FindPropertyRelative("sceneToLoad");
+
+                    if (EditorGUI.Toggle(new Rect(rect.x, rect.y, rect.width, EditorGUIUtility.singleLineHeight), "Current", Current.intValue == index, EditorStyles.radioButton))
+                    {
+                        Current.intValue = index;
+                    }
+                    rect.y += EditorGUIUtility.singleLineHeight;
+
+                    EditorGUI.PropertyField(new Rect(rect.x, rect.y, rect.width, EditorGUIUtility.singleLineHeight), Name);
+                    rect.y += EditorGUIUtility.singleLineHeight;
+                    EditorGUI.PropertyField(new Rect(rect.x, rect.y, rect.width, EditorGUIUtility.singleLineHeight), Slider);
+                    rect.y += EditorGUIUtility.singleLineHeight;
+                    EditorGUI.PropertyField(new Rect(rect.x, rect.y, rect.width, EditorGUIUtility.singleLineHeight), Buttons);
+                    rect.y += EditorGUIUtility.singleLineHeight;
+                    EditorGUI.PropertyField(new Rect(rect.x, rect.y, rect.width, EditorGUIUtility.singleLineHeight), SceneToLoad);
+                },
+                elementHeight = EditorGUIUtility.singleLineHeight * 5.5f,
+                onAddCallback = (list) =>
+                {
+                    var index = list.serializedProperty.arraySize;
+                    list.serializedProperty.arraySize++;
+                    list.index = index;
+
+                    var element = list.serializedProperty.GetArrayElementAtIndex(index);
+
+                    element.FindPropertyRelative("name").stringValue = null;
+                    element.FindPropertyRelative("showDifficultySlider").boolValue = false;
+                    element.FindPropertyRelative("buttonsToShow").objectReferenceValue = null;
+                },
+                onReorderCallbackWithDetails = (list, oldindex, newindex) =>
+                {
+                    if (Current.intValue == oldindex)
+                        Current.intValue = newindex;
+                    else
+                    if (Current.intValue < oldindex && Current.intValue >= newindex)
+                        Current.intValue++;
+                    else
+                    if (Current.intValue > oldindex && Current.intValue <= newindex)
+                        Current.intValue--;
+                }
+            };
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            ModesList.DoLayoutList();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/Scripts/RythmGame/Modes/Mode.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System;
+
+namespace DancingICE.Modes
+{
+    [Serializable]
+    public class Mode
+    {
+        public string name = null;
+        public bool showDifficultySlider = false;
+        public GameObject buttonsToShow = null;
+        public SceneField sceneToLoad = null;
+    }
+}
